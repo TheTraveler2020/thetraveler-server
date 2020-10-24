d1b2ecc8824f109fcd1ae02a9da22b5204ee3843
Nam-T <namthp99@gmail.com>
2020-10-24 22:54:24 +0700

    Tiny fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :authenticate_account!
+  # before_action :authenticate_account!
 
   # Chỉ thực hiện controller khi request thông qua API
   protect_from_forgery unless: -> { request.format.json? }
