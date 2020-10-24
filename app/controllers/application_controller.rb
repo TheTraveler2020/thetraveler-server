976b2ab0e3984a97a4ea5d555f2c62652d876232
Nam-T <namthp99@gmail.com>
2020-10-24 17:29:08 +0700

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_account!
 
-  # Chỉ thực hiện controller khi request thông qua API 
+  # Chỉ thực hiện controller khi request thông qua API
   protect_from_forgery unless: -> { request.format.json? }
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Account < ActiveRecord::Base
+class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
